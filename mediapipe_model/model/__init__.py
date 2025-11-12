4e0a3ea687983d295706356dbe9b37545a56635f
zhangkun <kun.zhang@wocheng.tv>
2025-11-12 16:27:36 +0800

    Complete mediapipe model code

---
+++ mediapipe_model/model/__init__.py
@@ -0,0 +1,2 @@
+from mediapipe_model.model.keypoint_classifier.keypoint_classifier import KeyPointClassifier
+from mediapipe_model.model.point_history_classifier.point_history_classifier import PointHistoryClassifier
---
+++ mediapipe_model/utils/__init__.py
@@ -0,0 +1,1 @@
+from mediapipe_model.utils.cvfpscalc import CvFpsCalc
